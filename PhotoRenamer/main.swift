182e1ba03e66583dd1369016374e0d51851bb266
Du Yizhuo <dyzdyz010@sina.com>
2015-04-11 21:48:28 +0800

    Delete redundant code

--- PhotoRenamer/main.swift
+++ PhotoRenamer/main.swift
@@ -8,20 +8,7 @@
 
 import Foundation
 
-func readDirectory(path: String) {
-    println("asdlkgjlasgj")
-    let fileManager = NSFileManager.defaultManager()
-    let enumerator = fileManager.enumeratorAtPath(path)
-    println("All object: \(enumerator?.allObjects)")
-    while let ele = enumerator?.nextObject() as? String {
-        
-    }
-}
-
-//println("First argument: \(Process.arguments)")
 var rawPath = ""
-var bundle = NSBundle.mainBundle()
-//println(bundle)
 if Process.argc == 1 {
     rawPath = NSBundle.mainBundle().bundlePath
 } else {
@@ -31,35 +18,5 @@ if Process.argc == 1 {
 process(rawPath)
 
 println("Total failed: \(failNum)")
-
-
-
-//let fileManager = NSFileManager.defaultManager()
-//let enumerator: NSDirectoryEnumerator = fileManager.enumeratorAtPath(path.absoluteString!)!
-//println(enumerator)
-//
-//while let ele = enumerator.nextObject() as? String {
-//    println("ele: ", ele)
-//}
-//
-//let img = CGImageSourceCreateWithURL(fullPath, nil)
-//if img == nil {
-//    println("File \(fullPath) doesn't exist.")
-//}
-//
-//let exif = CGImageSourceCopyPropertiesAtIndex(img, 0, nil) as NSDictionary
-////println(exif)
-//let tiff = exif[kCGImagePropertyTIFFDictionary as String] as? NSDictionary
-//println(tiff)
-//if let tiffDic = tiff {
-//    let datetimeStr = tiff![kCGImagePropertyTIFFDateTime as String] as! String
-//    println(datetimeStr)
-//    let dateFormater = NSDateFormatter()
-//    dateFormater.dateFormat = "yyyy:MM:dd HH:mm:ss"
-//    let date = dateFormater.dateFromString(datetimeStr)
-//    dateFormater.dateFormat = "yyyy-MM-dd HH.mm.ss"
-//    let newDateStr = dateFormater.stringFromDate(date!)
-//    println(newDateStr)
-//}
 
 
